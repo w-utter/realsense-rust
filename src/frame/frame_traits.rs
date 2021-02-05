1280b1e0aade6e49db89982444d248ba92e5ad5d
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-02-05 12:21:23 -0700

    Add thiserror crate to dependencies

--- src/frame/frame_traits.rs
+++ src/frame/frame_traits.rs
@@ -1,14 +1,23 @@
 //! Trait for describing basic frame operations
 
 use crate::common::*;
+use thiserror::Error;
 
+#[derive(Error, Debug)]
 pub enum FrameConstructionError {
+    #[error("Could not get frame width: {0}")]
     CouldNotGetWidth(String),
+    #[error("Could not get frame height: {0}")]
     CouldNotGetHeight(String),
+    #[error("Could not get stride: {0}")]
     CouldNotGetStride(String),
+    #[error("Could not get bits-per-pixel: {0}")]
     CouldNotGetBitsPerPixel(String),
+    #[error("Could not get frame stream profile: {0}")]
     CouldNotGetFrameStreamProfile(String),
+    #[error("Could not get data size (in bytes): {0}")]
     CouldNotGetDataSize(String),
+    #[error("Could not get pointer to frame data: {0}")]
     CouldNotGetData(String),
 }
 
