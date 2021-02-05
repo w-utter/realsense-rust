823f99756ee008fdedcfa95caf667f1edc25d0bb
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-02-05 13:59:13 -0700

    Move check_rs2_error macro to error.rs

--- src/error.rs
+++ src/error.rs
@@ -2,6 +2,23 @@
 
 use crate::common::*;
 
+#[doc(hidden)]
+#[macro_export]
+macro_rules! check_rs2_error {
+    ($rs2_error:expr, $result:expr) => {
+        // We make this alias here to type check $rs2_error.
+        let err: *mut sys::rs2_error = $rs2_error;
+        if NonNull::new(err).is_some() {
+            return Err($result(
+                std::ffi::CStr::from_ptr(sys::rs2_get_error_message(err))
+                    .to_str()
+                    .unwrap()
+                    .to_string(),
+            ));
+        }
+    };
+}
+
 #[derive(Debug)]
 pub(crate) struct ErrorChecker {
     checked: bool,
