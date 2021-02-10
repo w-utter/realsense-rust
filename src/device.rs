2814435dbe080d439f1e7eec056ee41ac1adb6a1
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-02-10 15:45:13 -0700

    Refactor Device type
    This is fairly similar to the changes made to Sensor, in that we
    refactor a couple key parts:
    
    - Remove the `first_*_sensor` methods. These can be trivially
    implemented in terms of getting the sensors from the sensor list.
    - Add the sensor list pointer to this struct type. This is so that we
    can properly drop it when the Device is dropped.
    - Change hardware_reset so that it consumes the type rather than borrow
    it.
    - Remove info accessors. They add noise to the class interface compared
    to just using `info`

--- src/device.rs
+++ src/device.rs
@@ -1,181 +1,138 @@
 //! Defines the device types.
 
-use crate::{
-    common::*,
-    error::{ErrorChecker, Result},
-    kind::Rs2CameraInfo,
-    sensor::{
-        ColorSensor, DepthSensor, DepthStereoSensor, FishEyeSensor, L500DepthSensor, MotionSensor,
-        PoseSensor, SoftwareSensor, Tm2Sensor,
-    },
-    sensor_list::SensorList,
-};
+use crate::{check_rs2_error, kind::Rs2CameraInfo, sensor::Sensor};
+use anyhow::Result;
+use num_traits::ToPrimitive;
+use realsense_sys as sys;
+use std::{convert::TryFrom, ffi::CStr, ptr::NonNull};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum DeviceConstructionError {
+    #[error("Could not create device from sensor. Reason: {0}")]
+    CouldNotCreateDeviceFromSensor(String),
+    #[error("Could not generate sensor list for device. Reason: {0}")]
+    CouldNotGenerateSensorList(String),
+}
 
 /// Represents a device instance.
 #[derive(Debug)]
 pub struct Device {
-    pub(crate) ptr: NonNull<sys::rs2_device>,
-}
-
-impl Device {
-    /// Discover available sensors on device.
-    pub fn sensors(&self) -> Result<SensorList> {
-        let list = unsafe {
-            let mut checker = ErrorChecker::new();
-            let ptr = sys::rs2_query_sensors(self.ptr.as_ptr(), checker.inner_mut_ptr());
-            checker.check()?;
-            SensorList::from_raw(ptr)
-        };
-        Ok(list)
-    }
-
-    pub fn hardware_reset(&self) -> Result<()> {
-        unsafe {
-            let mut checker = ErrorChecker::new();
-            sys::rs2_hardware_reset(self.ptr.as_ptr(), checker.inner_mut_ptr());
-            checker.check()?;
-        }
-        Ok(())
-    }
-
-    pub fn first_color_sensor(self) -> Result<Option<ColorSensor>> {
-        self.sensors()?.first_color_sensor()
-    }
-
-    pub fn first_depth_sensor(self) -> Result<Option<DepthSensor>> {
-        self.sensors()?.first_depth_sensor()
-    }
-
-    pub fn first_depth_stereo_sensor(self) -> Result<Option<DepthStereoSensor>> {
-        self.sensors()?.first_depth_stereo_sensor()
-    }
-
-    pub fn first_fish_eye_sensor(self) -> Result<Option<FishEyeSensor>> {
-        self.sensors()?.first_fish_eye_sensor()
-    }
-
-    pub fn first_l500_depth_sensor(self) -> Result<Option<L500DepthSensor>> {
-        self.sensors()?.first_l500_depth_sensor()
-    }
-
-    pub fn first_motion_sensor(self) -> Result<Option<MotionSensor>> {
-        self.sensors()?.first_motion_sensor()
-    }
-
-    pub fn first_pose_sensor(self) -> Result<Option<PoseSensor>> {
-        self.sensors()?.first_pose_sensor()
-    }
-
-    pub fn first_software_sensor(self) -> Result<Option<SoftwareSensor>> {
-        self.sensors()?.first_software_sensor()
-    }
-
-    pub fn first_tm2_sensor(self) -> Result<Option<Tm2Sensor>> {
-        self.sensors()?.first_tm2_sensor()
-    }
-
-    pub fn name(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::Name)
-    }
-
-    pub fn serial_number(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::SerialNumber)
-    }
-
-    pub fn recommended_firmware_version(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::RecommendedFirmwareVersion)
-    }
-
-    pub fn physical_port(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::PhysicalPort)
-    }
-
-    pub fn debug_op_code(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::DebugOpCode)
-    }
-
-    pub fn advanced_mode(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::AdvancedMode)
-    }
-
-    pub fn product_id(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::ProductId)
-    }
-
-    pub fn camera_locked(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::CameraLocked)
-    }
-
-    pub fn usb_type_descriptor(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::UsbTypeDescriptor)
-    }
-
-    pub fn product_line(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::ProductLine)
-    }
-
-    pub fn asic_serial_number(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::AsicSerialNumber)
-    }
-
-    pub fn firmware_update_id(&self) -> Result<Option<&str>> {
-        self.info(Rs2CameraInfo::FirmwareUpdateId)
-    }
-
-    pub fn info(&self, kind: Rs2CameraInfo) -> Result<Option<&str>> {
-        if !self.is_info_supported(kind)? {
-            return Ok(None);
-        }
-
-        let ptr = unsafe {
-            let mut checker = ErrorChecker::new();
-            let ptr = sys::rs2_get_device_info(
-                self.ptr.as_ptr(),
-                kind as sys::rs2_camera_info,
-                checker.inner_mut_ptr(),
-            );
-            checker.check()?;
-            ptr
-        };
-
-        // TODO: deallicate this CStr?
-        let string = unsafe { CStr::from_ptr(ptr).to_str().unwrap() };
-        Ok(Some(string))
-    }
-
-    pub fn is_info_supported(&self, kind: Rs2CameraInfo) -> Result<bool> {
-        let val = unsafe {
-            let mut checker = ErrorChecker::new();
-            let val = sys::rs2_supports_device_info(
-                self.ptr.as_ptr(),
-                kind as sys::rs2_camera_info,
-                checker.inner_mut_ptr(),
-            );
-            checker.check()?;
-            val
-        };
-        Ok(val != 0)
-    }
-
-    pub fn into_raw(self) -> *mut sys::rs2_device {
-        let ptr = self.ptr;
-        mem::forget(self);
-        ptr.as_ptr()
-    }
-
-    pub unsafe fn from_raw(ptr: *mut sys::rs2_device) -> Self {
-        Self {
-            ptr: NonNull::new(ptr).unwrap(),
-        }
-    }
+    device_ptr: NonNull<sys::rs2_device>,
+    sensor_list_ptr: NonNull<sys::rs2_sensor_list>,
 }
 
 impl Drop for Device {
     fn drop(&mut self) {
         unsafe {
-            sys::rs2_delete_device(self.ptr.as_ptr());
+            sys::rs2_delete_sensor_list(self.sensor_list_ptr.as_ptr());
+            sys::rs2_delete_device(self.device_ptr.as_ptr());
         }
     }
 }
 
 unsafe impl Send for Device {}
+
+impl TryFrom<NonNull<sys::rs2_device>> for Device {
+    type Error = DeviceConstructionError;
+
+    fn try_from(device_ptr: NonNull<sys::rs2_device>) -> Result<Self, Self::Error> {
+        unsafe {
+            let mut err = std::ptr::null_mut::<sys::rs2_error>();
+
+            let sensor_list = sys::rs2_query_sensors(device_ptr.as_ptr(), &mut err);
+            check_rs2_error!(err, DeviceConstructionError::CouldNotGenerateSensorList)?;
+
+            Ok(Device {
+                device_ptr,
+                sensor_list_ptr: NonNull::new(sensor_list).unwrap(),
+            })
+        }
+    }
+}
+
+impl Device {
+    /// Discover available sensors on device.
+    pub fn sensors(&self) -> Vec<Sensor> {
+        let mut sensors = Vec::new();
+        unsafe {
+            let mut err = std::ptr::null_mut::<sys::rs2_error>();
+
+            let len = sys::rs2_get_sensors_count(self.sensor_list_ptr.as_ptr(), &mut err);
+
+            if err.as_ref().is_some() {
+                return sensors;
+            }
+
+            for i in 0..len {
+                let sensor_ptr = sys::rs2_create_sensor(self.sensor_list_ptr.as_ptr(), i, &mut err);
+
+                if err.as_ref().is_some() {
+                    continue;
+                }
+
+                let nonnull_ptr = NonNull::new(sensor_ptr).unwrap();
+
+                match Sensor::try_from(nonnull_ptr) {
+                    Ok(s) => {
+                        sensors.push(s);
+                    }
+                    Err(_) => {
+                        continue;
+                    }
+                }
+            }
+        }
+        sensors
+    }
+
+    pub fn hardware_reset(self) {
+        unsafe {
+            let mut err = std::ptr::null_mut::<sys::rs2_error>();
+
+            // The only failure this can have is if device_ptr is null. This should not be the case
+            // since we're storing a `NonNull` type.
+            //
+            // It's a bit weird, but we don't need to actually check the error. Because if the
+            // devcie is null and this fails: you have an invalid device (so panic?) but if it
+            // succeeds, the device is no longer valid and we need to drop it. This is why this
+            // interface takes ownership of `self`.
+            sys::rs2_hardware_reset(self.device_ptr.as_ptr(), &mut err);
+        }
+    }
+
+    pub fn info(&self, camera_info: Rs2CameraInfo) -> Option<&CStr> {
+        if !self.supports_info(camera_info) {
+            return None;
+        }
+
+        unsafe {
+            let mut err = std::ptr::null_mut::<sys::rs2_error>();
+
+            let val = sys::rs2_get_device_info(
+                self.device_ptr.as_ptr(),
+                camera_info.to_u32().unwrap(),
+                &mut err,
+            );
+
+            if err.as_ref().is_some() {
+                None
+            } else {
+                Some(CStr::from_ptr(val))
+            }
+        }
+    }
+
+    pub fn supports_info(&self, camera_info: Rs2CameraInfo) -> bool {
+        unsafe {
+            let mut err = std::ptr::null_mut::<sys::rs2_error>();
+            let supports_info = sys::rs2_supports_device_info(
+                self.device_ptr.as_ptr(),
+                camera_info.to_u32().unwrap(),
+                &mut err,
+            );
+
+            err.as_ref().is_none() && supports_info != 0
+        }
+    }
+}
