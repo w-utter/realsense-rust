0417d6eb714d9c9c190ce425f43da0135fa49fc4
Brandon Minor <brandon@tangramvision.com>
2021-02-15 13:04:25 -0600

    Add rustdocs for Extension

--- src/kind/prelude.rs
+++ src/kind/prelude.rs
@@ -2,6 +2,8 @@
 
 use super::extension::Rs2Extension;
 
+/// A type describing the data held within an rs2_frame.
 pub trait Extension {
+    /// Identifies the proper RS2 extension for the type implementing this trait.
     fn extension() -> Rs2Extension;
 }
