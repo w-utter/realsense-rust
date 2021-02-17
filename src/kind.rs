2ec8273057c351a14440afcaebfe6240a5f34530
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-02-17 13:20:54 -0700

    Add rustdocs to kind module

--- src/kind.rs
+++ src/kind.rs
@@ -1,4 +1,32 @@
-//! Defines the common used enums.
+//! Contains commonly used enumerations and configuration types used across the crate.
+//!
+//! The `Kind` module holds a variety of types useful across realsense-rust APIs as transient
+//! values. The vast majority of types in this module are enums, which map to C-style enums in the
+//! lower-level `realsense-sys` bindings.
+//!
+//! # Architecture
+//!
+//! The low-level bindings for librealsense2 in `realsense-sys` represent the C-style enums as
+//! `u32` constants. They are wrapped / transformed into fully qualified types here so as to
+//! increase type safety across the API.
+//!
+//! All of these "wrapper" enums in Rust implement the [`ToPrimitive`](num_traits::ToPrimitive) and
+//! [`FromPrimitive`](num_traits::FromPrimitive) traits from the `num_traits` crate. If you need to
+//! access the original enum value, you can do so with the following code:
+//!
+//! ```rust
+//! use num_traits::ToPrimitive;
+//! use crate::kind::Rs2Extension;
+//!
+//! fn main() {
+//!     let ext = Rs2Extension::ColorSensor;
+//!     println!("The extension is: {}", ext.to_u32().unwrap());
+//! }
+//! ```
+//!
+//! In practice, most of the time you shouldn't need to wrap or unwrap `u32` values, and the API
+//! should never spit one out at you.
+//!
 
 mod camera_info;
 mod color_scheme;
