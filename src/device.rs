ad722fdb4da8de7c9adc93fce95e6fdb6679b9c6
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-02-24 10:33:29 -0700

    Remove sensor list member from device
    This is safe to query dynamically, and the sensors generated from this
    list can be safely retained even after the list has been dropped.

--- src/device.rs
+++ src/device.rs
@@ -13,7 +13,7 @@ use crate::{
 use anyhow::Result;
 use num_traits::ToPrimitive;
 use realsense_sys as sys;
-use std::{convert::TryFrom, ffi::CStr, ptr::NonNull};
+use std::{convert::From, ffi::CStr, ptr::NonNull};
 use thiserror::Error;
 
 /// Enumeration of possible errors that can occur during device construction
@@ -22,9 +22,6 @@ pub enum DeviceConstructionError {
     /// System was unable to get the device pointer that corresponds to a given [`Sensor`]
     #[error("Could not create device from sensor. Type: {0}; Reason: {1}")]
     CouldNotCreateDeviceFromSensor(Rs2Exception, String),
-    /// Could not generate the sensor list corresponding to the device during construction.
-    #[error("Could not generate sensor list for device. Type: {0}; Reason: {1}")]
-    CouldNotGenerateSensorList(Rs2Exception, String),
     /// Could not get device from device list
     #[error("Could not get device from device list. Type: {0}; Reason: {1}")]
     CouldNotGetDeviceFromDeviceList(Rs2Exception, String),
@@ -41,13 +38,11 @@ pub enum DeviceConstructionError {
 #[derive(Debug)]
 pub struct Device {
     device_ptr: NonNull<sys::rs2_device>,
-    sensor_list_ptr: NonNull<sys::rs2_sensor_list>,
 }
 
 impl Drop for Device {
     fn drop(&mut self) {
         unsafe {
-            sys::rs2_delete_sensor_list(self.sensor_list_ptr.as_ptr());
             sys::rs2_delete_device(self.device_ptr.as_ptr());
         }
     }
@@ -55,31 +50,13 @@ impl Drop for Device {
 
 unsafe impl Send for Device {}
 
-impl TryFrom<NonNull<sys::rs2_device>> for Device {
-    type Error = DeviceConstructionError;
-
+impl From<NonNull<sys::rs2_device>> for Device {
     /// Attempt to construct a Device from a non-null pointer to `rs2_device`.
     ///
-    /// Constructs a device from a pointer to an `rs2_device` type from the C-FFI, or returns an
-    /// error if the device and its corresponding sensor list cannot be obtained.
+    /// Constructs a device from a pointer to an `rs2_device` type from the C-FFI.
     ///
-    /// # Errors
-    ///
-    /// Returns [`DeviceConstructionError::CouldNotGenerateSensorList`] if the sensor list cannot
-    /// be captured during construction.
-    ///
-    fn try_from(device_ptr: NonNull<sys::rs2_device>) -> Result<Self, Self::Error> {
-        unsafe {
-            let mut err = std::ptr::null_mut::<sys::rs2_error>();
-
-            let sensor_list = sys::rs2_query_sensors(device_ptr.as_ptr(), &mut err);
-            check_rs2_error!(err, DeviceConstructionError::CouldNotGenerateSensorList)?;
-
-            Ok(Device {
-                device_ptr,
-                sensor_list_ptr: NonNull::new(sensor_list).unwrap(),
-            })
-        }
+    fn from(device_ptr: NonNull<sys::rs2_device>) -> Self {
+        Device { device_ptr }
     }
 }
 
@@ -110,7 +87,7 @@ impl Device {
             )?;
 
             let nonnull_device_ptr = NonNull::new(device_ptr).unwrap();
-            Device::try_from(nonnull_device_ptr)
+            Ok(Device::from(nonnull_device_ptr))
         }
     }
 
@@ -120,18 +97,28 @@ impl Device {
     /// This can occur if the physical device is disconnected before this call is made.
     ///
     pub fn sensors(&self) -> Vec<Sensor> {
-        let mut sensors = Vec::new();
         unsafe {
-            let mut err = std::ptr::null_mut::<sys::rs2_error>();
+            let mut sensors = Vec::new();
 
-            let len = sys::rs2_get_sensors_count(self.sensor_list_ptr.as_ptr(), &mut err);
+            let mut err = std::ptr::null_mut::<sys::rs2_error>();
+            let sensor_list_ptr = sys::rs2_query_sensors(self.device_ptr.as_ptr(), &mut err);
 
             if err.as_ref().is_some() {
                 return sensors;
             }
 
+            let nonnull_sensor_list = NonNull::new(sensor_list_ptr).unwrap();
+
+            let len = sys::rs2_get_sensors_count(nonnull_sensor_list.as_ptr(), &mut err);
+
+            if err.as_ref().is_some() {
+                sys::rs2_delete_sensor_list(nonnull_sensor_list.as_ptr());
+                return sensors;
+            }
+
+            sensors.reserve(len as usize);
             for i in 0..len {
-                match Sensor::try_create(&self.sensor_list_ptr, i) {
+                match Sensor::try_create(&nonnull_sensor_list, i) {
                     Ok(s) => {
                         sensors.push(s);
                     }
@@ -140,8 +127,9 @@ impl Device {
                     }
                 }
             }
+            sys::rs2_delete_sensor_list(nonnull_sensor_list.as_ptr());
+            sensors
         }
-        sensors
     }
 
     /// Takes ownership of the device and forces a hardware reset on the device.
