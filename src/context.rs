640c6d7a0f0da24d0cb2064b1e65d3fd11cdfe55
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-02-24 10:53:25 -0700

    Add rustdocs for Context module and struct

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,4 @@
-//! Defines the sensor context.
+//! Type that defines a RealSense context used by the rest of the API
 
 use crate::{
     base::from_path,
@@ -10,26 +10,31 @@ use crate::{
 use anyhow::Result;
 use num_traits::ToPrimitive;
 use realsense_sys as sys;
-use std::{path::Path, ptr::NonNull};
+use std::{convert::From, path::Path, ptr::NonNull};
 use thiserror::Error;
 
+/// Type describing a RealSense context, used by the rest of the API.
 #[derive(Debug)]
 pub struct Context {
     context_ptr: NonNull<sys::rs2_context>,
 }
 
+/// An error type describing failure to construct a context.
 #[derive(Error, Debug)]
 #[error("Could not construct the context. Type: {0}; Reason: {1}")]
 pub struct ContextConstructionError(pub Rs2Exception, pub String);
 
+/// An error type describing failure to get the device hub from a context.
 #[derive(Error, Debug)]
 #[error("Could not get the device hub from the context. Type: {0}; Reason: {1}")]
 pub struct CouldNotGetDeviceHubError(pub Rs2Exception, pub String);
 
+/// An error type describing failure to add a device from a file.
 #[derive(Error, Debug)]
 #[error("Could not add a device from file. Type: {0}; Reason: {1}")]
 pub struct CouldNotAddDeviceError(pub Rs2Exception, pub String);
 
+/// An error type describing failure to remove a device from a file.
 #[derive(Error, Debug)]
 #[error("Could not remove device from file. Type: {0}; Reason: {1}")]
 pub struct CouldNotRemoveDeviceError(pub Rs2Exception, pub String);
@@ -43,8 +48,13 @@ impl Drop for Context {
 unsafe impl Send for Context {}
 
 impl Context {
-    /// Create an instance.
-    pub fn new() -> Result<Self> {
+    /// Construct a new context.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`ContextConstructionError`] if the context cannot be created.
+    ///
+    pub fn new() -> Result<Self, ContextConstructionError> {
         unsafe {
             let mut err = std::ptr::null_mut::<sys::rs2_error>();
             let ptr = sys::rs2_create_context(sys::RS2_API_VERSION as i32, &mut err);
@@ -56,8 +66,13 @@ impl Context {
         }
     }
 
-    /// Create an [DeviceHub](DeviceHub) instance.
-    pub fn create_device_hub(&self) -> Result<DeviceHub> {
+    /// Creates a device hub from the context.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`CouldNotGetDeviceHubError`] if the device hub cannot be created.
+    ///
+    pub fn create_device_hub(&self) -> Result<DeviceHub, CouldNotGetDeviceHubError> {
         unsafe {
             let mut err = std::ptr::null_mut::<sys::rs2_error>();
             let devicehub_ptr = sys::rs2_create_device_hub(self.context_ptr.as_ptr(), &mut err);
@@ -67,7 +82,7 @@ impl Context {
         }
     }
 
-    /// Discover available devices.
+    /// Get a list of devices that are already connected to the host.
     pub fn query_devices(&self, product_mask: Vec<Rs2ProductLine>) -> Vec<Device> {
         // TODO/TEST: Make sure that an empty mask (therefore giving no filter) gives
         // us _all_ devices, not _no_ devices.
@@ -113,21 +128,44 @@ impl Context {
         devices
     }
 
-    /// Add device file to context.
-    pub fn add_device<P>(&mut self, file: P) -> Result<()>
+    /// Create a new device and add it to the context.
+    ///
+    /// This adds a "device" at a particular file on the system to the RealSense context. Returns a
+    /// handle to the device, or an error if this call fails.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`NulError`](std::ffi::NulError) if the provided file path cannot be cleanly
+    /// represented as a [`CString`](std::ffi::CString). This usually only occurs if you have null
+    /// characters in the path. Constructing a path using the utilties in Rust's [`std::fs`] are
+    /// expected to work.
+    ///
+    /// Returns [`CouldNotAddDeviceError`] if the device cannot be added.
+    ///
+    pub fn add_device<P>(&mut self, file: P) -> Result<Device>
     where
         P: AsRef<Path>,
     {
         let path = from_path(file)?;
         unsafe {
             let mut err = std::ptr::null_mut::<sys::rs2_error>();
-            sys::rs2_context_add_device(self.context_ptr.as_ptr(), path.as_ptr(), &mut err);
+            let device_ptr =
+                sys::rs2_context_add_device(self.context_ptr.as_ptr(), path.as_ptr(), &mut err);
             check_rs2_error!(err, CouldNotAddDeviceError)?;
 
-            Ok(())
+            Ok(Device::from(NonNull::new(device_ptr).unwrap()))
         }
     }
 
+    /// Removes a playback device from the context, if it exists
+    ///
+    /// This removes a "device" at a particular file on the system from the RealSense context.
+    /// Returns nothing (null tuple) or an Error if the device cannot be removed.
+    ///
+    /// # Errors
+    ///
+    /// Returns [`CouldNotRemoveDeviceError`] if the device cannot be removed for any reason.
+    ///
     pub fn remove_device<P>(&mut self, file: P) -> Result<()>
     where
         P: AsRef<Path>,
@@ -142,7 +180,15 @@ impl Context {
         }
     }
 
-    pub unsafe fn get_raw(&self) -> NonNull<sys::rs2_context> {
+    /// Get the underlying low-level pointer to the context object.
+    ///
+    /// # Safety
+    ///
+    /// This method is not intended to be called or used outside of the crate itself. Be warned, it
+    /// is _undefined behaviour_ to call [`realsense_sys::rs2_delete_context`] on this pointer. If
+    /// you do, you risk a double-free error when the [`Context`] struct itself is dropped.
+    ///
+    pub(crate) unsafe fn get_raw(&self) -> NonNull<sys::rs2_context> {
         self.context_ptr
     }
 }
