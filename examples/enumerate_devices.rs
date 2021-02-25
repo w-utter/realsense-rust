4072ef7623791d509f34b66ed6c00cfc9dea639f
Brandon Minor <brandon@tangramvision.com>
2021-02-25 09:47:25 -0600

    Create enumerate_devices example
    This is a modification of the old "reset_devices" example. It has been updated
    with the new API calls for info and context. It also prints more information
    about the system when called.

---
+++ examples/enumerate_devices.rs
@@ -0,0 +1,39 @@
+use anyhow::{ensure, Result};
+use realsense_rust::{
+    context::Context,
+    device::Device,
+    kind::{Rs2CameraInfo, Rs2ProductLine},
+};
+use std::collections::HashSet;
+
+fn match_info(device: &Device, info_param: Rs2CameraInfo) -> String {
+    match device.info(info_param) {
+        Some(s) => String::from(s.to_str().unwrap()),
+        None => String::from("N/A"),
+    }
+}
+
+fn main() -> Result<()> {
+    println!("----\nEnumerating all devices compatible with RealSense:\n----");
+    let mut queried_devices = HashSet::new();
+    queried_devices.insert(Rs2ProductLine::Any);
+
+    // If there are no devices, Vec will be length 0
+    let devices = Context::new()?.query_devices(queried_devices);
+
+    ensure!(!devices.is_empty(), "No devices found");
+
+    for device in devices {
+        let name = match_info(&device, Rs2CameraInfo::Name);
+        let sn = match_info(&device, Rs2CameraInfo::SerialNumber);
+        let fw = match_info(&device, Rs2CameraInfo::FirmwareVersion);
+        let rec_fw = match_info(&device, Rs2CameraInfo::RecommendedFirmwareVersion);
+        println!(
+            ">  {:25} | SN: {:15} | Curr Fw Ver: {:15} | Rec FW Ver: {:15}",
+            name, sn, fw, rec_fw
+        );
+        device.hardware_reset();
+    }
+    println!("---");
+    Ok(())
+}
