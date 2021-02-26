a68f23a054a60fc02c551fe521652835499ebd84
Jeremy Steward <jeremy.steward@tangramvision.com>
2021-02-26 15:17:43 -0700

    Add test for resolving color & depth config on d400 series

--- tests/connectivity.rs
+++ tests/connectivity.rs
@@ -2,13 +2,13 @@
 
 #![cfg(feature = "test-single-device")]
 
-use anyhow::Result;
 use realsense_rust::{
+    config::Config,
     context::Context,
-    device::Device,
-    kind::{Rs2CameraInfo, Rs2ProductLine},
+    kind::{Rs2CameraInfo, Rs2Format, Rs2ProductLine, Rs2StreamKind},
+    pipeline::InactivePipeline,
 };
-use std::collections::HashSet;
+use std::{collections::HashSet, convert::TryFrom};
 
 /// Ensure at least one intel device is "connected" as far as the driver is concerned.
 ///
@@ -22,4 +22,48 @@ fn ensure_at_least_one_intel_device_connected() {
     let devices = context.query_devices(mask);
 
     assert!(devices.len() >= 1);
+}
+
+#[test]
+fn can_resolve_all_streams_always() {
+    let context = Context::new().unwrap();
+    let mut config = Config::new();
+    config.enable_all_streams().unwrap();
+
+    let pipeline = InactivePipeline::try_from(&context).unwrap();
+
+    assert!(pipeline.can_resolve(&config));
+}
+
+#[test]
+fn can_resolve_color_and_depth_on_d400_series() {
+    let context = Context::new().unwrap();
+    let devices = context.query_devices(HashSet::new());
+
+    let device = devices.iter().find(|d| {
+        let serial = d.info(Rs2CameraInfo::ProductLine).unwrap();
+        serial.to_str().unwrap() == "D400"
+    });
+
+    if let Some(device) = device {
+        let serial = device.info(Rs2CameraInfo::SerialNumber).unwrap();
+        let mut config = Config::new();
+
+        config
+            .enable_device_from_serial(serial)
+            .unwrap()
+            .disable_all_streams()
+            .unwrap()
+            .enable_stream(Rs2StreamKind::Color, 0, 640, 0, Rs2Format::Rgba8, 30)
+            .unwrap()
+            .enable_stream(Rs2StreamKind::Depth, 0, 640, 0, Rs2Format::Z16, 30)
+            .unwrap();
+
+        let pipeline = InactivePipeline::try_from(&context).unwrap();
+
+        assert!(pipeline.can_resolve(&config));
+        assert!(pipeline.resolve(&config).is_some());
+    } else {
+        return;
+    }
 }
