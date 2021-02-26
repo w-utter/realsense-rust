4c4591bcd45c2a323c7e678aaa41873d7bc5efff
Brandon Minor <brandon@tangramvision.com>
2021-02-26 16:15:44 -0600

    Complete first drafts of 435i and L515 demos

--- examples/demo_l515.rs
+++ examples/demo_l515.rs
@@ -6,9 +6,12 @@ use realsense_rust::{
     kind::{Rs2Format, Rs2ProductLine, Rs2StreamKind},
     pipeline::InactivePipeline,
 };
-use std::collections::HashSet;
-use std::convert::TryFrom;
-use std::time::Duration;
+use std::{
+    collections::HashSet,
+    convert::TryFrom,
+    io::{self, Write},
+    time::Duration,
+};
 
 pub fn main() -> Result<()> {
     // Check for depth or color-compatible devices.
@@ -16,19 +19,25 @@ pub fn main() -> Result<()> {
     queried_devices.insert(Rs2ProductLine::Any);
     let context = Context::new()?;
     let devices = context.query_devices(queried_devices);
-    ensure!(!devices.is_empty(), "No devices found");
+    ensure!(!devices.is_empty(), "No devices found.");
 
     // create pipeline
     let pipeline = InactivePipeline::try_from(&context)?;
     let mut config = Config::new();
-    config.enable_stream(Rs2StreamKind::Depth, 0, 640, 0, Rs2Format::Z16, 30)?;
-    config.enable_stream(Rs2StreamKind::Color, 0, 640, 0, Rs2Format::Rgb8, 30)?;
+    config.enable_stream(Rs2StreamKind::Depth, 0, 320, 0, Rs2Format::Z16, 30)?;
+    config.enable_stream(Rs2StreamKind::Infrared, 0, 320, 0, Rs2Format::Y8, 30)?;
+    if !pipeline.can_resolve(&config) {
+        println!("Cannot resolve assigned config. Check the config for incompaible types.");
+        return Ok(());
+    }
     let mut pipeline = pipeline.start(Some(&config))?;
 
     // process frames
     for _ in 0..1000 {
-        let timeout = Duration::from_millis(1000);
+        // The L515 takes a while to initialize. Make this 5 seconds.
+        let timeout = Duration::from_millis(5000);
         let frames = pipeline.wait(Some(timeout))?;
+
         let mut depth_frames = frames.frames_of_extension::<DepthFrame>();
         if depth_frames.is_empty() {
             continue;
@@ -36,9 +45,12 @@ pub fn main() -> Result<()> {
 
         // Debug width and height calls
         let depth_frame = depth_frames.pop().unwrap();
-        // println!("{:#?}", depth_frame);
         let distance = depth_frame.distance(depth_frame.width() / 2, depth_frame.height() / 2)?;
-        print!("\rCurrent distance of center pixel: {:15} m", distance);
+        if distance == 0.0 {
+            continue;
+        }
+        print!("\rCurrent distance of center pixel: {:<15} m", distance);
+        io::stdout().flush().unwrap();
     }
 
     Ok(())
