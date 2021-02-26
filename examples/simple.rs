27863e3277275ea396c3cf094920f40bf1b44c39
Brandon Minor <brandon@tangramvision.com>
2021-02-26 10:49:41 -0600

    Simplify the "simple" example
    Instead of storing everything into a frame, this merely starts streaming and
    reports the middle pixel value of the depth frame. This gets rid of some
    dependencies while leaving the functionality (on the realsense side)

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,69 +1,52 @@
-use anyhow::Result;
+use anyhow::{ensure, Result};
+use realsense_rust::{
+    config::Config,
+    context::Context,
+    frame::{DepthFrame, FrameEx, VideoFrame},
+    kind::{Rs2Extension, Rs2Format, Rs2ProductLine, Rs2StreamKind},
+    pipeline::{ActivePipeline, InactivePipeline},
+};
+use std::collections::HashSet;
+use std::convert::TryFrom;
+use std::time::Duration;
 
-#[cfg(all(feature = "with-image", feature = "with-nalgebra"))]
-mod example {
-    use anyhow::Result;
-    use image::ImageFormat;
-    use realsense_rust::{prelude::*, Config, Pipeline, Resolution, Rs2Format, Rs2StreamKind};
-    use std::time::Duration;
+pub fn main() -> Result<()> {
+    // Check for depth or color-compatible devices.
+    let mut queried_devices = HashSet::new();
+    queried_devices.insert(Rs2ProductLine::Any);
+    let context = Context::new()?;
+    let devices = context.query_devices(queried_devices);
+    ensure!(!devices.is_empty(), "No devices found");
 
-    pub fn main() -> Result<()> {
-        // create pipeline
-        let pipeline = Pipeline::new()?;
-        let config = Config::new()?
-            .enable_stream(Rs2StreamKind::Depth, 0, 640, 0, Rs2Format::Z16, 30)?
-            .enable_stream(Rs2StreamKind::Color, 0, 640, 0, Rs2Format::Rgb8, 30)?;
-        let mut pipeline = pipeline.start(&config)?;
+    // print!("\rProcessing {}%...", i);
+    // // or
+    // // stdout.write(format!("\rProcessindevice::Device,g {}%...", i).as_bytes()).unwrap();
 
-        // process frames
-        for _ in 0..1000 {
-            let timeout = Duration::from_millis(1000);
-            let frames = match pipeline.wait(timeout)? {
-                Some(frames) => frames,
-                None => {
-                    println!("timeout error");
-                    continue;
-                }
-            };
+    // stdout.flush().unwrap();
+    // sleep(Duration::from_millis(20));
 
-            println!("frame number = {}", frames.number()?);
+    // create pipeline
+    let pipeline = InactivePipeline::try_from(&context)?;
+    let mut config = Config::new();
+    config.enable_stream(Rs2StreamKind::Depth, 0, 640, 0, Rs2Format::Z16, 30)?;
+    config.enable_stream(Rs2StreamKind::Color, 0, 640, 0, Rs2Format::Rgb8, 30)?;
+    let mut pipeline = pipeline.start(Some(&config))?;
 
-            let color_frame = frames.color_frame()?.unwrap();
-            let depth_frame = frames.depth_frame()?.unwrap();
-
-            // save video frame
-            {
-                let image = color_frame.owned_image()?;
-                image.save_with_format(
-                    format!("sync-video-example-{}.png", color_frame.number()?),
-                    ImageFormat::Png,
-                )?;
-            }
-
-            // save depth frame
-            {
-                let Resolution { width, height } = depth_frame.resolution()?;
-                let distance = depth_frame.distance(width / 2, height / 2)?;
-                println!("distance = {}", distance);
-
-                let image = depth_frame.owned_image()?;
-                image.save_with_format(
-                    format!("sync-depth-example-{}.png", depth_frame.number()?),
-                    ImageFormat::Png,
-                )?;
-            }
+    // process frames
+    for _ in 0..1000 {
+        let timeout = Duration::from_millis(1000);
+        let frames = pipeline.wait(Some(timeout))?;
+        let mut depth_frames = frames.frames_of_extension::<DepthFrame>();
+        if depth_frames.is_empty() {
+            continue;
         }
 
-        Ok(())
+        // Debug width and height calls
+        let depth_frame = depth_frames.pop().unwrap();
+        println!("{:#?}", depth_frame);
+        let distance = depth_frame.distance(depth_frame.width(), depth_frame.height())?;
+        print!("\rCurrent distance of center pixel: {:15} m", distance);
     }
-}
 
-#[cfg(all(feature = "with-image", feature = "with-nalgebra"))]
-fn main() -> Result<()> {
-    example::main()
-}
-
-#[cfg(not(all(feature = "with-image", feature = "with-nalgebra")))]
-fn main() {
-    panic!("please enable with-image and with-nalgebra features to run this example");
+    Ok(())
 }
