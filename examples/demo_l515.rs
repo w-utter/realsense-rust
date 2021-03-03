245d061ef54b49cc7c78d7ed706ced5a0bdd21d6
Brandon Minor <brandon@tangramvision.com>
2021-03-03 17:19:30 -0600

    Add USB check and config branch to demo examples

--- examples/demo_l515.rs
+++ examples/demo_l515.rs
@@ -1,3 +1,10 @@
+//! Configure and stream a L515 sensor.
+//!
+//! Notice that the streaming configuration changes based on the USB speed of the sensor.
+//! If one attemps to set a streaming configuration that is too much for the current USB
+//! speed, RealSense will return with an error. However, that error is non-descript and will
+//! not help identify the underlying problem, i.e. the bandwidth of the connection.
+
 use anyhow::{ensure, Result};
 use realsense_rust::{
     config::Config,
@@ -24,11 +31,27 @@ pub fn main() -> Result<()> {
     // Create pipeline
     let pipeline = InactivePipeline::try_from(&context)?;
     let mut config = Config::new();
-    config
-        .enable_device_from_serial(devices[0].info(Rs2CameraInfo::SerialNumber).unwrap())?
-        .disable_all_streams()?
-        .enable_stream(Rs2StreamKind::Depth, None, 320, 0, Rs2Format::Z16, 30)?
-        .enable_stream(Rs2StreamKind::Infrared, None, 320, 0, Rs2Format::Y8, 30)?;
+
+    // Check the USB speed of our connection
+    // CStr => str => f32
+    let usb_cstr = devices[0].info(Rs2CameraInfo::UsbTypeDescriptor).unwrap();
+    let usb_val: f32 = usb_cstr.to_str().unwrap().parse().unwrap();
+    if usb_val > 3.0 {
+        config
+            .enable_device_from_serial(devices[0].info(Rs2CameraInfo::SerialNumber).unwrap())?
+            .disable_all_streams()?
+            .enable_stream(Rs2StreamKind::Depth, None, 640, 0, Rs2Format::Z16, 30)?
+            .enable_stream(Rs2StreamKind::Infrared, None, 640, 0, Rs2Format::Y8, 30)?
+            .enable_stream(Rs2StreamKind::Gyro, None, 0, 0, Rs2Format::Any, 0)?;
+    } else {
+        config
+            .enable_device_from_serial(devices[0].info(Rs2CameraInfo::SerialNumber).unwrap())?
+            .disable_all_streams()?
+            .enable_stream(Rs2StreamKind::Depth, None, 320, 0, Rs2Format::Z16, 30)?
+            .enable_stream(Rs2StreamKind::Infrared, None, 320, 0, Rs2Format::Y8, 30)?
+            .enable_stream(Rs2StreamKind::Gyro, None, 0, 0, Rs2Format::Any, 0)?;
+    }
+
     // Change pipeline's type from InactivePipeline -> ActivePipeline
     let mut pipeline = pipeline.start(Some(&config))?;
 
