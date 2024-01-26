f7c9ebd36f4da6d9c4cd56fee78605dc1dc9a2a2
Brandon Minor <brandon@tangramvision.com>
2024-01-26 14:11:07 -0800

    Add lints to realsense-sys manifest

--- realsense-sys/src/lib.rs
+++ realsense-sys/src/lib.rs
@@ -1,8 +1,8 @@
+#![doc = include_str!("../README.md")]
 // Allow all warnings here -- Bindgen generates this file, we really don't care about individual
 // warnings since we can't really do much about them, we'd have to fix bindgen upstream or
 // librealsense2 itself.
 #![allow(warnings)]
+#![allow(missing_docs)]
 #![allow(clippy::missing_docs_in_private_items)]
-#![doc = include_str!("../README.md")]
-
 include!("../bindings/bindings.rs");
